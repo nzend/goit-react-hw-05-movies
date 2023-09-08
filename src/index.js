ca4bfcc668c199e5eb593252306052ee7f23c7d5
nzend <115723110+nzend@users.noreply.github.com>
2023-09-08 23:19:56 +0300

    add vercel config

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </React.StrictMode>
